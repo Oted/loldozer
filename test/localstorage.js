e644ea10d3f380664104e20f468aeac726e8ce04
Oted <herr.olle@gmail.com>
2015-04-19 20:03:51 +0100

    update localstorage test

--- test/localstorage.js
+++ test/localstorage.js
@@ -10,17 +10,7 @@ var describe    = lab.describe,
     expect      = Code.expect;
 
 var Storage         = require('../js/utils/localstorage'),
-    testStorage     = require('./data/test_storage.json'),
-    testValues  = {
-        "all" : {
-            "first" : 4,
-            "last" : 3
-        },
-        "youtube" : {
-            "first" : 2,
-            "last" : 1
-        }
-    };
+    testStorage     = require('./data/test_storage.json');
 
 
 describe('storage', function () {
@@ -28,18 +18,17 @@ describe('storage', function () {
         var testPerformers = [
             {
                 'type' : "all",
-                '_sort' : 10
+                '_sort' : 1429444601612
             },
             {
                 "type"  : 'all',
-                "_sort" : 9
+                "_sort" : 1429444455402
             }
         ];
 
         Storage.updateSession(testPerformers);
-        expect(Storage.getStorage()).to.not.exist();
-        expect(Storage.getSession()["all"]["first"]).to.equal(10);
-        expect(Storage.getSession()["all"]["last"]).to.equal(9);
+        expect(Storage.getSession()["all"]["first"]).to.equal(1429444601612);
+        expect(Storage.getSession()["all"]["last"]).to.equal(1429444455402);
         done();
     });
     
@@ -47,94 +36,77 @@ describe('storage', function () {
         var testPerformers = [
             {
                 'type' : "all",
-                '_sort' : 100
-            },
-            {
-                "type"  : 'all',
-                "_sort" : 2
+                '_sort' : 1429444433871
             }
         ];
 
         Storage.updateSession(testPerformers);
-        expect(Storage.getStorage()).to.not.exist();
-        expect(Storage.getSession()["all"]["first"]).to.equal(100);
-        expect(Storage.getSession()["all"]["last"]).to.equal(2);
-        done();
-    });
-
-    it('should update the all and the lol key in session', function (done) {
-        var testPerformers = [
-            {
-                'type' : "all",
-                '_sort' : 100
-            },
-            {
-                "type"  : 'all',
-                "_sort" : 2
-            },
-            {
-                "type" : "lol",
-                "_sort" : 1
-            }
-        ];
-
-        Storage.updateSession(testPerformers);
-        expect(Storage.getStorage()).to.not.exist();
-        expect(Storage.getSession()["all"]["first"]).to.equal(100);
-        expect(Storage.getSession()["all"]["last"]).to.equal(1);
-        expect(Storage.getSession()["lol"]["first"]).to.equal(1);
-        expect(Storage.getSession()["lol"]["last"]).to.equal(1);
-
+        expect(Storage.getSession()["all"]["first"]).to.equal(1429444601612);
+        expect(Storage.getSession()["all"]["last"]).to.equal(1429444433871);
         done();
     });
 
     it('should update the storage', function (done) {
         Storage.mergeAndUpdateStorage();
-        
         expect(Storage.getStorage()["all"].length).to.equal(1);
-        expect(Storage.getStorage()["lol"].length).to.equal(1);
-        expect(Storage.getStorage()["all"][0].last).to.equal(1);
-
+        expect(Storage.getStorage()["all"][0].last).to.equal(1429444433871);
         done();
     });
     
     it('should update the storage once more', function (done) {
+        Storage.destroySession();
         var testPerformers = [
             {
                 'type' : "all",
-                '_sort' : 200
+                '_sort' : 1429444373494
             },
             {
                 "type"  : 'all',
-                "_sort" : 150
+                "_sort" : 1429444372337
             },
             {
-                "type" : "lol",
-                "_sort" : 2
+                "type" : "all",
+                "_sort" : 1429444367676
             }
         ];
 
         Storage.updateSession(testPerformers);
-        expect(Storage.getSession()["all"]["first"]).to.equal(200);
-        expect(Storage.getSession()["all"]["last"]).to.equal(1);
-        expect(Storage.getSession()["lol"]["first"]).to.equal(2);
-        expect(Storage.getSession()["lol"]["last"]).to.equal(1);
-
+        Storage.mergeAndUpdateStorage();
+        expect(Storage.getStorage()["all"][0].last).to.equal(1429444433871);
+        expect(Storage.getStorage()["all"][1].last).to.equal(1429444367676);
         done();
     });
     
     it('should update the storage', function (done) {
-        Storage.mergeAndUpdateStorage();
-        
-        expect(Storage.getStorage()["all"].length).to.equal(1);
-        expect(Storage.getStorage()["lol"].length).to.equal(1);
-        expect(Storage.getStorage()["all"][0].first).to.equal(200);
-        expect(Storage.getStorage()["all"][0].last).to.equal(1);
+        Storage.destroySession();
+        var testPerformers = [
+            {
+                'type' : "all",
+                '_sort' : 1429444217809
+            },
+            {
+                "type"  : 'all',
+                "_sort" : 1429444187569
+            },
+            {
+                "type" : "all",
+                "_sort" : 1429444173196
+            }
+        ];
 
+        Storage.updateSession(testPerformers);
+        Storage.mergeAndUpdateStorage();
+        console.log('STORAGE',Storage.getStorage());
+        expect(Storage.getStorage()["all"][0].first).to.equal(1429444601612);
+        expect(Storage.getStorage()["all"][0].last).to.equal(1429444367676);
+        expect(Storage.getStorage()["all"][1].first).to.equal(1429444217809);
+        expect(Storage.getStorage()["all"][1].last).to.equal(1429444173196);
         done();
     });
     
     it('should update the storage once more', function (done) {
+        console.log('----')
+            process.exit();
         var testPerformers = [
             {
                 'type' : "all",
@@ -148,7 +120,7 @@ describe('storage', function () {
 
         Storage.updateSession(testPerformers);
         Storage.mergeAndUpdateStorage();
-        expect(Storage.getSession()["all"]["first"]).to.equal(200);
+        expect(Storage.getSession()["all"]["first"]).to.equal(400);
         expect(Storage.getSession()["all"]["last"]).to.equal(1);
         expect(Storage.getSession()["lol"]["first"]).to.equal(2);
         expect(Storage.getSession()["lol"]["last"]).to.equal(1);
